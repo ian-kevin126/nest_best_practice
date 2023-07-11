b85e11edde8bf938cd685fa4612e6893b3f1329e
ian-kevin126 <kevinliao125@163.com>
2023-07-11 22:51:54 +0800

    feat: test git

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 /*
  * @Author: ian-kevin126 kevinliao125@163.com
+ * @Author: ian-kevin126 kevinliao125@163.com
  * @Date: 2023-07-10 23:43:42
  * @LastEditors: ian-kevin126 kevinliao125@163.com
- * @LastEditTime: 2023-07-11 22:48:51
+ * @LastEditTime: 2023-07-11 22:51:48
  * @FilePath: /nest_best_practice/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
